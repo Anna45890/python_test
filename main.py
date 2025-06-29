8ec04159498f40a0fb4d66799dbbc69d9b265a30
Anna Chickmareva <annasobennikovav@gmail.com>
2025-06-29 09:59:00 +0300

    play "ramdom number"

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("Hello, World!")
+import random
+
+print("Привет! Я загадала число от 1 до 100.")
+print("Попробуй угадать! У тебя 10 попыток.")
+
+secret_number = random.randint(1, 100)
+
+attempts = 0
+
+while attempts < 10:
+    your_number = int(input("Введи число: "))
+    attempts += 1
+    if your_number > secret_number:
+        print("Число слишком большое.")
+    elif your_number < secret_number:
+        print("Число слишком маленькое.")
+    else:
+        print("Поздравляю! Вы угадали число!")
+        att_num.append(attempts)
+        print(f"Количество попыток: {attempts}")
+        break
+else:
+    print(f"Вы не угадали число. Это было {secret_number}")
